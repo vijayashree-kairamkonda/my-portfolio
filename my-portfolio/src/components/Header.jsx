e5d3c7a81beb004888da2e72a1bbf7d58e33e082
kairamkonda vijayasree <75258563+vijayashree-kairamkonda@users.noreply.github.com>
2023-11-18 20:47:27 +0530

    Update Header.jsx

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,13 +1,13 @@
 const Header = () => {
   return (
     <div className="flex flex-wrap w-screen bg-purple-700 justify-between p-6 sticky cursor-pointer">
-      <div className="hover:text-white font-semibold">VIJAYASHREE K</div>
-      <div className="flex space-x-16 flex-wrap">
+      <div className="text-white text-xl font-semibold">VIJAYASHREE K</div>
+      {/* <div className="flex space-x-16 flex-wrap">
         <span className="hover:text-white">Home</span>
         <span className="hover:text-white">About</span>
         <span className="hover:text-white">Projects</span>
         <span className="hover:text-white">Contact</span>
-      </div>
+      </div> */}
     </div>
   );
 };
